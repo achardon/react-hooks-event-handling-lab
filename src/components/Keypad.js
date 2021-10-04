9c24abb9db0ceafa37b64779ebe1efce1f697d73
Alessandra Chardon <achardon@ucla.edu>
2021-10-04 15:13:12 -0400

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,8 @@
 // Code Keypad Component Here
+function Keypad () {
+    return (
+        <input onChange={() => console.log('Entering password...')}type='password'></input>
+    );
+}
+
+export default Keypad;
